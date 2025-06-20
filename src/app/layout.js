df1b065dec42a04698b6778dffca141eb9e20741
ShairAli10 <shairaliyamin@gmail.com>
2025-06-21 02:31:31 +0500

    added distance parameter in searchdetail Api

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+// app/layout.js
+
 import "./globals.css";
 import Header from "@/app/(components)/Header";
 import Footer from "@/app/(components)/Footer";
@@ -5,12 +7,13 @@ import { AppContextProvider } from "@/context/appContext";
 import RootStyles from "./RootStyles";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Script from "next/script"; 
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Fonts and Favicon */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -22,7 +25,8 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.png" />
-         {/* Google tag (gtag.js) */}
+
+        {/* Google tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-BXCCJV6N44"
           strategy="afterInteractive"
@@ -39,11 +43,59 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
+
+        {/* HelpScout Beacon */}
+        <Script
+          id="helpscout-beacon-loader"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n){
+                function a(){
+                  var e=t.getElementsByTagName("script")[0],
+                      n=t.createElement("script");
+                  n.type="text/javascript",
+                  n.async=!0,
+                  n.src="https://beacon-v2.helpscout.net",
+                  e.parentNode.insertBefore(n,e)
+                }
+                if(e.Beacon=n=function(t,n,a){
+                  e.Beacon.readyQueue.push({method:t,options:n,data:a})
+                },
+                n.readyQueue=[],
+                "complete"===t.readyState)
+                  return a();
+                e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)
+              }(window,document,window.Beacon||function(){});
+            `,
+          }}
+        />
+        <Script
+          id="helpscout-beacon-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.Beacon('init', '8e008e76-12be-44d7-a367-55b01efa2d78');
+            `,
+          }}
+        />
+
+        {/* OMAPI Script */}
         <Script
           id="omapi-loader"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,366531,387306);`,
+            __html: `
+              (function(d,u,ac){
+                var s=d.createElement('script');
+                s.type='text/javascript';
+                s.src='https://a.omappapi.com/app/js/api.min.js';
+                s.async=true;
+                s.dataset.user=u;
+                s.dataset.account=ac;
+                d.getElementsByTagName('head')[0].appendChild(s);
+              })(document,366531,387306);
+            `,
           }}
         />
       </head>
